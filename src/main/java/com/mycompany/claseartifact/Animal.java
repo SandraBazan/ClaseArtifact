357f1f2c7ef484a1eb125efb62a52edc8916a882
Sandra <sandra.bj0308@gmail.com>
2020-01-31 21:09:25 +0100

    Creación de la clase Animal, sus atributos, métodos y constructor parametrizado.

---
+++ src/main/java/com/mycompany/claseartifact/Animal.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.claseartifact;
+
+/**
+ *
+ * @author San
+ */
+public class Animal {
+    private String especie, raza, nombre;
+    private int peso;
+
+    public Animal(String especie, String raza, String nombre, int peso) {
+        this.especie = especie;
+        this.raza = raza;
+        this.nombre = nombre;
+        this.peso = peso;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() {
+        return "El animal se llama: " + nombre + "\nPesa: " + peso + "\nEs de la especie: " + especie + " y de la raza: " + raza + ".";
+    }
+}
